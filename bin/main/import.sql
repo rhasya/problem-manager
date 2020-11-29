9bc247016f375258018cf8ee28611935422ddb18
snow <rhasya@gmail.com>
2020-11-29 19:30:12 +0900

    add gitignore

--- bin/main/import.sql
+++
@@ -1,4 +0,0 @@
-insert into author (KOR_NAME, ENG_NAME) values ('선호', '');
-insert into author (KOR_NAME, ENG_NAME) values ('영호', '');
-insert into author (KOR_NAME, ENG_NAME) values ('민호', '');
-insert into author (KOR_NAME, ENG_NAME) values ('정호', '');
