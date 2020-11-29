7b39c360423f54714359248b00857213f303859c
snow <rhasya@gmail.com>
2020-11-29 23:22:49 +0900

    change login url

--- src/main/java/com/snow/probmgmt/config/WebSecurityConfig.java
+++ src/main/java/com/snow/probmgmt/config/WebSecurityConfig.java
@@ -44,7 +44,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticationEntryPoint(authenticationEntryPoint)
                 .and()
             .formLogin()
-                .loginProcessingUrl("/login").permitAll()
+                .loginProcessingUrl("/api/admin/login").permitAll()
                 .successHandler(successHandler)
                 .and()
             .logout()
